45e772c065600bc502fb34020561920c4d676686
Victoria Mercouris <vnmercouris@wpi.edu>
2025-01-29 19:18:13 -0500

    New setup to Give Repeat Calls to Function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="Test TeleOp" , group="Linear OpMode")
 public class Test extends LinearOpMode {
     Robot robot = new Robot();  // Using Robot.Java class to interface with the robot's hardware
+    int target = 0;
 
     @Override
     public void runOpMode() {
@@ -17,9 +18,11 @@ public class Test extends LinearOpMode {
 
         // Main control loop that runs while the OpMode is active
         while (opModeIsActive()) {
+            // New setup allows for repeat calls of the function
              if (gamepad1.b){
-                 robot.proportionalControlMotor(robot.intakeArm,1000);
+                 target = 1000;
              }
+            robot.proportionalControlMotor(robot.intakeArm,1000);
 
         }
     }
