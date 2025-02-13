99415b4430b606b11be8bf8be6fabfa771374b8d
LARobotics <LARobotics@lacademy.edu>
2025-02-13 16:09:49 -0500

    Update AutoBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBot.java
@@ -10,13 +10,13 @@ public class AutoBot extends Robot {
     }
 
     //                          in cm
-    public void moveToPosition(int targetPosition, double power, double timeoutS) {
+    public void moveToPosition(double cm, double power, double timeoutS) {
         // Reset encoders after position movement
 
-        int newLeftFrontTarget = leftFront.getCurrentPosition()+getTicksPerCm(targetPosition);
-        int newRightFrontTarget = rightFront.getCurrentPosition()+getTicksPerCm(targetPosition);
-        int newLeftBackTarget = leftBack.getCurrentPosition()+getTicksPerCm(targetPosition);
-        int newRightBackTarget = rightBack.getCurrentPosition()+getTicksPerCm(targetPosition);
+        int newLeftFrontTarget = leftFront.getCurrentPosition()+getTicksPerCm(cm);
+        int newRightFrontTarget = rightFront.getCurrentPosition()+getTicksPerCm(cm);
+        int newLeftBackTarget = leftBack.getCurrentPosition()+getTicksPerCm(cm);
+        int newRightBackTarget = rightBack.getCurrentPosition()+getTicksPerCm(cm);
 
 
         leftFront.setTargetPosition(newLeftFrontTarget);
@@ -55,6 +55,54 @@ public class AutoBot extends Robot {
         rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+    }
+
+    /*
+   This function uses the encoders to strafe left or right.
+   Negative input for inches results in left strafing.
+    */
+    public void strafeToPosition(double cm, double speed, double timeoutS){
+
+
+        int newLeftFrontTarget = leftFront.getCurrentPosition()+getTicksPerCm(cm);
+        int newRightFrontTarget = rightFront.getCurrentPosition()-getTicksPerCm(cm);
+        int newLeftBackTarget = leftBack.getCurrentPosition()-getTicksPerCm(cm);
+        int newRightBackTarget = rightBack.getCurrentPosition()+getTicksPerCm(cm);
+        leftFront.setTargetPosition(newLeftFrontTarget);
+        leftBack.setTargetPosition(newRightFrontTarget);
+        rightFront.setTargetPosition(newLeftBackTarget);
+        rightBack.setTargetPosition(newRightBackTarget);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        super.period.reset();
+        leftFront.setPower(Math.abs(speed));
+        leftBack.setPower(Math.abs(speed));
+        rightFront.setPower(Math.abs(speed));
+        rightBack.setPower(Math.abs(speed));
+
+        while (period.seconds() < timeoutS &&
+                leftFront.isBusy() && rightFront.isBusy() && leftBack.isBusy() && rightBack.isBusy()) {
+
+            // Telemetry for debugging
+            /*telemetry.addData("Target Positions", "%7D :%7D",
+                    newLeftFrontTarget, newRightFrontTarget, newLeftBackTarget, newRightBackTarget);
+            telemetry.addData("Current Positions", "%7D :%7D ",
+                    leftFront.getCurrentPosition(), rightFront.getCurrentPosition(),
+                    leftBack.getCurrentPosition(), rightBack.getCurrentPosition());
+            telemetry.addData("Time Elapsed", "", period.seconds());
+            telemetry.update();*/
+        }
+
+        stopDriveTrain();
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public int getTicksPerCm(double cm) {
