36cf6128b56db12ca0596461ff4415dfe292c4b6
Jayce Chen <160561031+JayceChen19@users.noreply.github.com>
2025-02-21 17:00:57 -0500

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNear.java
@@ -13,14 +13,34 @@ public class AutoNear extends LinearOpMode {
 
         waitForStart();
 
-        bot.drive(-1, 0, 0, 0, false);
-        sleep(1000);
-        bot.drive(0, -1, 0, 0, false);
+        bot.frictionBasedGrabber.setPosition(110/180);
         sleep(500);
-        bot.drive(1, 0, 0, 0, false);
-        sleep(2000);
+        bot.smplScoringBucket.setPosition(RobotState.BASE.getBucketPosition());
+        sleep(1000);
+
+        bot.drive(-1, 0, 0, 0, false);
+        sleep(1200);
+        bot.drive(0, 0, 0, 1, false);
+        sleep(300);
+        bot.drive(0, -1, 0, 0, false);
+        sleep(300);
+        bot.stopDriveTrain();
+        while(bot.linearSlide.getCurrentPosition()<RobotState.SMPL_SETUP.getSlidePosition() ){
+            bot.proportionalControlMotor(bot.linearSlide, RobotState.SMPL_SETUP.getSlidePosition(), 0.008);
+        }
+        sleep(500);
+        bot.smplScoringBucket.setPosition(RobotState.SMPL_SCORE.getBucketPosition());
+        sleep(500);
+        while(bot.linearSlide.getCurrentPosition()>RobotState.BASE.getSlidePosition() ){
+            bot.proportionalControlMotor(bot.linearSlide, RobotState.BASE.getSlidePosition(), 0.008);
+        }
+        sleep(500);
         bot.drive(0, 1, 0, 0, false);
-        sleep(700);
+        sleep(300);
+        bot.drive(0, 0, 1,0 , false);
+        sleep(230);
+        bot.drive(1, 0, 0, 0, false);
+        sleep(3400);
         bot.stopDriveTrain();
 
     }
